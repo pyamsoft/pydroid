ab1dfe236acd29a90395c52d88b39ed2e0ac145a
pyamsoft <pyam.soft@gmail.com>
2016-08-14 11:10:22 -0700

    Add a base fragment with ActionBar

---
+++ src/main/java/com/pyamsoft/pydroid/base/fragment/ActionBarFragment.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.pydroid.base.fragment;
+
+import android.support.annotation.CheckResult;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public abstract class ActionBarFragment extends Fragment {
+
+  @CheckResult @Nullable protected ActionBar getActionBar() {
+    final FragmentActivity activity = getActivity();
+    if (activity instanceof AppCompatActivity) {
+      final AppCompatActivity appCompatActivity = (AppCompatActivity) activity;
+      return appCompatActivity.getSupportActionBar();
+    } else {
+      throw new ClassCastException("Activity not instance of AppCompatActivity");
+    }
+  }
+
+  protected void setActionBar(@Nullable Toolbar toolbar) {
+    final FragmentActivity activity = getActivity();
+    if (activity instanceof AppCompatActivity) {
+      final AppCompatActivity appCompatActivity = (AppCompatActivity) activity;
+      appCompatActivity.setSupportActionBar(toolbar);
+    } else {
+      throw new ClassCastException("Activity not instance of AppCompatActivity");
+    }
+  }
+
+  protected void setActionBarUpEnabled(boolean up) {
+    final ActionBar bar = getActionBar();
+    if (bar != null) {
+      bar.setHomeButtonEnabled(up);
+      bar.setDisplayHomeAsUpEnabled(up);
+    }
+  }
+}

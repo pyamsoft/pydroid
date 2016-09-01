cf90e01e2434da19023cb1210e78a7bcd3366da5
pyamsoft <pyam.soft@gmail.com>
2016-09-01 00:10:33 -0700

    Add tests for the persistent cache

--- src/test/java/com/pyamsoft/pydroid/util/PersistentCacheTest.java
+++ src/test/java/com/pyamsoft/pydroid/util/PersistentCacheTest.java
@@ -16,16 +16,201 @@
 
 package com.pyamsoft.pydroid.util;
 
+import android.os.Bundle;
+import android.support.annotation.CheckResult;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import com.pyamsoft.pydroid.BuildConfig;
+import com.pyamsoft.pydroid.base.app.PersistLoader;
+import com.pyamsoft.pydroid.base.presenter.PresenterBase;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class) @Config(constants = BuildConfig.class, sdk = 23)
 public class PersistentCacheTest {
 
-  @Test public void test_cacheLoad() {
+  @Nullable private final Bundle NULL_STATE = null;
 
+  /**
+   * Test that we generate two different keys when instances are not saved
+   */
+  @Test public void test_generateDifferent() {
+    // Generate first key
+    final String instance1 = "i1";
+    final long firstResult = PersistentCache.generateKey(instance1, NULL_STATE);
+
+    // Generate second key
+    final String instance2 = "i2";
+    final long secondResult = PersistentCache.generateKey(instance2, NULL_STATE);
+
+    Assert.assertNotEquals(firstResult, secondResult);
+  }
+
+  /**
+   * Test that we generate the same key when instances are saved
+   */
+  @Test public void test_generateSame() {
+    // Generate first key
+    final String instance = "i1";
+    final long firstResult = PersistentCache.generateKey(instance, NULL_STATE);
+
+    // Save the key
+    final Bundle outState = new Bundle();
+    PersistentCache.saveKey(instance, outState, firstResult);
+
+    // Fetch the key again
+    final long secondResult = PersistentCache.generateKey(instance, outState);
+
+    Assert.assertEquals(firstResult, secondResult);
+  }
+
+  /**
+   * To be used with test_loadSynchronous
+   */
+  @CheckResult long doLoadSynchronous(@NonNull String instance, @Nullable Bundle instanceState,
+      @NonNull AtomicInteger createCount, @NonNull AtomicInteger loadCount) {
+    return PersistentCache.load(instance, instanceState, new PersistLoader.Callback<Object>() {
+      @NonNull @Override public PersistLoader<Object> createLoader() {
+        return new PersistLoader<Object>(RuntimeEnvironment.application) {
+          @NonNull @Override public Object loadPersistent() {
+            createCount.getAndIncrement();
+            return createCount;
+          }
+        };
+      }
+
+      @Override public void onPersistentLoaded(@NonNull Object persist) {
+        loadCount.getAndIncrement();
+      }
+    });
+  }
+
+  /**
+   * Test that the cache loads the same key when passed in the same values, and that objects are
+   * only created a single time and then cached
+   */
+  @Test public void test_loadSynchronous() {
+    // Keeps track of the number of new creates
+    final AtomicInteger createCount = new AtomicInteger(0);
+    // Keeps track of the number of total loads
+    final AtomicInteger loadCount = new AtomicInteger(0);
+
+    // Generate first key
+    final String instance = "i1";
+
+    // First generate will not have a saved state
+    final long loadedKey = doLoadSynchronous(instance, null, createCount, loadCount);
+
+    // Synchronous so we can check here
+    Assert.assertEquals(createCount.get(), loadCount.get());
+
+    // Now save state
+    final Bundle outState = new Bundle();
+    PersistentCache.saveKey(instance, outState, loadedKey);
+
+    // Reload
+    final long newKey = doLoadSynchronous(instance, outState, createCount, loadCount);
+
+    // Check that keys are the same
+    Assert.assertEquals(loadedKey, newKey);
+
+    // Check that counts are different
+    Assert.assertNotEquals(createCount.get(), loadCount.get());
+
+    // Check that load count is more than create
+    Assert.assertTrue(loadCount.get() > createCount.get());
+  }
+
+  /**
+   * Test that clean up properly destroys a destroyable object like a presenter
+   */
+  @Test public void test_cleanup() {
+    // First load a presenter
+    final TestPresenter[] presenterHack = new TestPresenter[1];
+    final long loadedKey =
+        PersistentCache.load("I", null, new PersistLoader.Callback<TestPresenter>() {
+          @NonNull @Override public PersistLoader<TestPresenter> createLoader() {
+            return new PersistLoader<TestPresenter>(RuntimeEnvironment.application) {
+              @NonNull @Override public TestPresenter loadPersistent() {
+                return new TestPresenter();
+              }
+            };
+          }
+
+          @Override public void onPersistentLoaded(@NonNull TestPresenter persist) {
+            presenterHack[0] = persist;
+          }
+        });
+
+    // Make sure it is not destroyed
+    final TestPresenter testPresenter = presenterHack[0];
+    Assert.assertFalse(testPresenter.isDestroyed());
+
+    // Unload the presenter
+    PersistentCache.unload(loadedKey);
+
+    // Check again
+    Assert.assertTrue(testPresenter.isDestroyed());
+  }
+
+  /**
+   * Test that clean up does not destroy an object if it does not implement the Destroyable
+   * interface
+   */
+  @Test public void test_doNotDestroy() {
+    // First load a presenter
+    final DoNotDestroy[] hack = new DoNotDestroy[1];
+    final long loadedKey =
+        PersistentCache.load("I", null, new PersistLoader.Callback<DoNotDestroy>() {
+          @NonNull @Override public PersistLoader<DoNotDestroy> createLoader() {
+            return new PersistLoader<DoNotDestroy>(RuntimeEnvironment.application) {
+              @NonNull @Override public DoNotDestroy loadPersistent() {
+                return new DoNotDestroy();
+              }
+            };
+          }
+
+          @Override public void onPersistentLoaded(@NonNull DoNotDestroy persist) {
+            hack[0] = persist;
+          }
+        });
+
+    // Make sure it is not destroyed
+    final DoNotDestroy doNotDestroy = hack[0];
+    Assert.assertFalse(doNotDestroy.isDestroyed());
+
+    // Unload the presenter
+    PersistentCache.unload(loadedKey);
+
+    // Check again
+    Assert.assertFalse(doNotDestroy.isDestroyed());
+  }
+
+  static class DoNotDestroy {
+
+    private boolean destroyed = false;
+
+    public boolean isDestroyed() {
+      return destroyed;
+    }
+  }
+
+  static class TestPresenter extends PresenterBase<String> {
+
+    private boolean destroyed = false;
+
+    public boolean isDestroyed() {
+      return destroyed;
+    }
+
+    @Override protected void onDestroy() {
+      super.onDestroy();
+      destroyed = true;
+    }
   }
 }

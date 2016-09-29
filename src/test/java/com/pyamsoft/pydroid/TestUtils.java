3e7a6d29721730e8b089b53ee4ce44fb2e033bf6
pyamsoft <pyam.soft@gmail.com>
2016-09-28 22:10:17 -0700

    Move test utils

--- src/test/java/com/pyamsoft/pydroid/TestUtils.java
+++ src/test/java/com/pyamsoft/pydroid/TestUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.pyamsoft.pydroid.util;
+package com.pyamsoft.pydroid;
 
 import android.annotation.SuppressLint;
 import android.support.annotation.CheckResult;
@@ -23,7 +23,7 @@ import android.support.v7.app.AppCompatActivity;
 import org.robolectric.Robolectric;
 import org.robolectric.util.ActivityController;
 
-final class TestUtils {
+public final class TestUtils {
 
   private TestUtils() {
     throw new RuntimeException("No instances");

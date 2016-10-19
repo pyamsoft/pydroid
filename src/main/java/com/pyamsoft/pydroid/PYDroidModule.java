659f740f003f82da53ab6c507195022290787fbf
pyamsoft <pyam.soft@gmail.com>
2016-10-18 23:31:02 -0700

    Move billing into a singleton module
    Billing should only be created once. Each time billing is created it attempts
    to connect to Google Play In App service. Create it and connect only once
    to save on resources

--- src/main/java/com/pyamsoft/pydroid/PYDroidModule.java
+++ src/main/java/com/pyamsoft/pydroid/PYDroidModule.java
@@ -17,6 +17,7 @@
 package com.pyamsoft.pydroid;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
@@ -26,13 +27,20 @@ import com.pyamsoft.pydroid.social.SocialMediaModule;
 import com.pyamsoft.pydroid.support.SupportModule;
 import com.pyamsoft.pydroid.version.ApiModule;
 import com.pyamsoft.pydroid.version.VersionCheckModule;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.solovyev.android.checkout.Billing;
+import org.solovyev.android.checkout.Purchase;
+import org.solovyev.android.checkout.PurchaseVerifier;
+import org.solovyev.android.checkout.RequestListener;
 
 public class PYDroidModule {
 
   @NonNull private final Provider provider;
 
-  PYDroidModule(@NonNull Context context) {
-    provider = new Provider(context);
+  PYDroidModule(@NonNull Application application) {
+    provider = new Provider(application);
   }
 
   // Create a new one every time
@@ -64,16 +72,101 @@ public class PYDroidModule {
 
   public static class Provider {
 
+    @NonNull private static final String SKU_DONATE = ".donate";
+    @NonNull private static final String SKU_DONATE_ONE = SKU_DONATE + ".one";
+    @NonNull private static final String SKU_DONATE_TWO = SKU_DONATE + ".two";
+    @NonNull private static final String SKU_DONATE_FIVE = SKU_DONATE + ".five";
+    @NonNull private static final String SKU_DONATE_TEN = SKU_DONATE + ".ten";
+
     // Singleton
     @NonNull private final Context appContext;
+    @NonNull private final Billing billing;
+    @NonNull private final List<String> inAppPurchaseList;
 
-    Provider(final @NonNull Context context) {
-      appContext = context.getApplicationContext();
+    Provider(final @NonNull Application application) {
+      appContext = application.getApplicationContext();
+      inAppPurchaseList = createInAppPurchaseList(application);
+      billing =
+          new Billing(appContext, new DonationBillingConfiguration(appContext.getPackageName()));
+    }
+
+    @CheckResult @NonNull List<String> createInAppPurchaseList(@NonNull Context context) {
+      final Context appContext = context.getApplicationContext();
+      final String packageName = appContext.getPackageName();
+      final String appSpecificSkuDonateOne = packageName + SKU_DONATE_ONE;
+      final String appSpecificSkuDonateTwo = packageName + SKU_DONATE_TWO;
+      final String appSpecificSkuDonateFive = packageName + SKU_DONATE_FIVE;
+      final String appSpecificSkuDonateTen = packageName + SKU_DONATE_TEN;
+
+      final List<String> skuList = new ArrayList<>();
+      skuList.add(appSpecificSkuDonateOne);
+      skuList.add(appSpecificSkuDonateTwo);
+      skuList.add(appSpecificSkuDonateFive);
+      skuList.add(appSpecificSkuDonateTen);
+
+      if (BuildConfig.DEBUG) {
+        skuList.add("android.test.purchased");
+        skuList.add("android.test.canceled");
+        skuList.add("android.test.refunded");
+        skuList.add("android.test.item_unavailable");
+      }
+
+      return Collections.unmodifiableList(skuList);
     }
 
     // Singleton
     @CheckResult @NonNull public final Context provideContext() {
       return appContext;
+    }
+
+    // Singleton
+    @CheckResult @NonNull public final Billing provideBilling() {
+      return billing;
+    }
+
+    // Singleton
+    @CheckResult @NonNull public final List<String> provideInAppPurchaseList() {
+      return inAppPurchaseList;
+    }
+
+    @SuppressWarnings("WeakerAccess") static class DonationBillingConfiguration
+        extends Billing.DefaultConfiguration {
+
+      @NonNull private final String publicKey;
+
+      DonationBillingConfiguration(@NonNull String publicKey) {
+        this.publicKey = publicKey;
+      }
+
+      @NonNull @Override public String getPublicKey() {
+        return publicKey;
+      }
+
+      /**
+       * We do not really need any purchase verification as they are all just donations anyway.
+       * Our public key is not used, so the default verifier fails anyway.
+       *
+       * Pass a verifier which always passes
+       */
+      @NonNull @Override public PurchaseVerifier getPurchaseVerifier() {
+        return new AlwaysPurchaseVerifier();
+      }
+    }
+
+    @SuppressWarnings("WeakerAccess") static class AlwaysPurchaseVerifier
+        implements PurchaseVerifier {
+
+      /**
+       * Verify all purchases as 'valid'
+       */
+      @Override public void verify(@NonNull List<Purchase> purchases,
+          @NonNull RequestListener<List<Purchase>> listener) {
+        final List<Purchase> verifiedPurchases = new ArrayList<>(purchases.size());
+        for (Purchase purchase : purchases) {
+          verifiedPurchases.add(purchase);
+        }
+        listener.onSuccess(verifiedPurchases);
+      }
     }
   }
 }

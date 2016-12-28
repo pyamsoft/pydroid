b57c4a367f3dd21aa8471f43810f92519ffb0605
pyamsoft <pyam.soft@gmail.com>
2016-12-28 13:12:57 -0800

    Revert back to loading inventory
    Crash on emulator is reported upstream, no real thing we can do about it for now

--- pydroid/src/main/java/com/pyamsoft/pydroid/donate/RealCheckout.java
+++ pydroid/src/main/java/com/pyamsoft/pydroid/donate/RealCheckout.java
@@ -61,15 +61,6 @@ class RealCheckout implements ICheckout {
     }
   }
 
-  private void makeInventory() {
-    checkCheckoutNonNull();
-
-    if (inventory == null) {
-      //noinspection ConstantConditions
-      inventory = checkout.makeInventory();
-    }
-  }
-
   @Override public void createForActivity(@NonNull Activity activity) {
     if (checkout != null) {
       throw new IllegalStateException("Checkout is already created for a different Activity");
@@ -92,12 +83,16 @@ class RealCheckout implements ICheckout {
     //noinspection ConstantConditions
     checkout.start();
     checkout.createPurchaseFlow(new DonationPurchaseListener());
-    makeInventory();
+    loadInventory();
   }
 
   @Override public void loadInventory() {
     checkCheckoutNonNull();
-    makeInventory();
+
+    if (inventory == null) {
+      //noinspection ConstantConditions
+      inventory = checkout.makeInventory();
+    }
 
     if (inventoryCallback != null) {
       //noinspection ConstantConditions

fb8f1b5c2cbe5f2574b291c05c7152bc910f88eb
pyamsoft <pyam.soft@gmail.com>
2017-06-21 15:42:45 -0700

    Add ViewPresenter and SchedulerViewPresenter for simple click events

---
+++ pydroid-ui/src/main/java/com/pyamsoft/pydroid/ui/presenter/SchedulerViewPresenter.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.pydroid.ui.presenter
+
+import android.support.annotation.CallSuper
+import android.view.View
+import android.widget.CompoundButton
+import com.pyamsoft.pydroid.presenter.SchedulerPresenter
+import io.reactivex.Scheduler
+
+abstract class SchedulerViewPresenter(foregroundScheduler: Scheduler,
+    backgroundScheduler: Scheduler) : SchedulerPresenter(foregroundScheduler,
+    backgroundScheduler), ViewPresenterContract {
+
+  private val delegate = DelegateViewPresenter()
+
+  final override fun clickEvent(view: View, func: (View) -> Unit) {
+    delegate.clickEvent(view, func)
+  }
+
+  final override fun checkChangedEvent(view: CompoundButton,
+      func: (CompoundButton, Boolean) -> Unit) {
+    delegate.checkChangedEvent(view, func)
+  }
+
+  @CallSuper override fun onStop() {
+    super.onStop()
+    delegate.stop()
+  }
+
+  @CallSuper override fun onDestroy() {
+    super.onDestroy()
+    delegate.destroy()
+  }
+
+  private class DelegateViewPresenter : ViewPresenter()
+
+}
+

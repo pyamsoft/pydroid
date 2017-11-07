cdaedc036e444553dbb433bce684681f23d9cb70
pyamsoft <pyam.soft@gmail.com>
2017-11-06 19:39:49 -0800

    Ship the social media layout in an app settings fragment

---
+++ pydroid-ui/src/main/java/com/pyamsoft/pydroid/ui/app/fragment/AppSettingsFragment.kt
@@ -0,0 +1,63 @@
+/*
+ *     Copyright (C) 2017 Peter Kenji Yamanaka
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc.,
+ *     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.pyamsoft.pydroid.ui.app.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pyamsoft.pydroid.ui.R
+import com.pyamsoft.pydroid.ui.databinding.FragmentAppSettingsBinding
+
+abstract class AppSettingsFragment : DisposableFragment() {
+
+  private lateinit var binding: FragmentAppSettingsBinding
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+      savedInstanceState: Bundle?): View? {
+    binding = FragmentAppSettingsBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    binding.unbind()
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    showPreferenceFragment()
+  }
+
+  private fun showPreferenceFragment() {
+    val fragmentManager = childFragmentManager
+    val tag: String = provideSettingsTag()
+    if (fragmentManager.findFragmentByTag(tag) == null) {
+      fragmentManager.beginTransaction()
+          .replace(R.id.app_settings_content, provideSettingsFragment(), tag)
+          .commit()
+    }
+  }
+
+  abstract fun provideSettingsFragment(): ActionBarSettingsPreferenceFragment
+
+  abstract fun provideSettingsTag(): String
+
+}
+

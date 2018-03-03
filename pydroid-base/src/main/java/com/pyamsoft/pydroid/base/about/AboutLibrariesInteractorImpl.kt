41a5c58c0218c5925672ca4e1c6fc639ea96ab04
pyamsoft <pyam.soft@gmail.com>
2018-03-03 06:44:22 -0800

    Clean up interactor

--- pydroid-base/src/main/java/com/pyamsoft/pydroid/base/about/AboutLibrariesInteractorImpl.kt
+++ pydroid-base/src/main/java/com/pyamsoft/pydroid/base/about/AboutLibrariesInteractorImpl.kt
@@ -25,37 +25,31 @@ internal class AboutLibrariesInteractorImpl internal constructor(
   private val dataSource: AboutLibrariesDataSource
 ) : AboutLibrariesInteractor {
 
-  override fun loadLicenses(force: Boolean): Observable<AboutLibrariesModel> {
-    return Observable.defer {
-      Observable.fromIterable(Licenses.getLicenses())
-    }
-        .toSortedList { (name1), (name2) ->
-          name1.compareTo(name2)
-        }
-        .flatMapObservable { Observable.fromIterable(it) }
-        .concatMap { model ->
-          loadLicenseText(model).flatMapObservable {
-            Observable.just(
-                AboutLibrariesModel.create(
-                    model.name, model.homepage, it
-                )
-            )
-          }
-        }
+  @CheckResult
+  private fun createLicenseStream(): Observable<AboutLibrariesModel> {
+    return Observable.defer { Observable.fromIterable(Licenses.getLicenses()) }
   }
 
   @CheckResult
   private fun loadLicenseText(model: AboutLibrariesModel): Single<String> {
     return Single.fromCallable<String> {
-      val name = model.name
-
       if (model.customContent.isEmpty()) {
-        Timber.d("Load from asset location: %s (%s)", name, model.license)
         return@fromCallable dataSource.loadNewLicense(model.license)
       } else {
-        Timber.d("License: %s provides custom content", name)
         return@fromCallable model.customContent
       }
     }
+  }
+
+  @CheckResult
+  private fun loadTextForAboutModel(model: AboutLibrariesModel): Observable<AboutLibrariesModel> {
+    return loadLicenseText(model).map { AboutLibrariesModel.create(model.name, model.homepage, it) }
+        .toObservable()
+  }
+
+  override fun loadLicenses(force: Boolean): Observable<AboutLibrariesModel> {
+    return createLicenseStream()
+        .sorted { o1, o2 -> o1.name.compareTo(o2.name, ignoreCase = true) }
+        .concatMap { loadTextForAboutModel(it) }
   }
 }

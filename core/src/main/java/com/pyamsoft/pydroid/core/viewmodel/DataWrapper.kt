15f89e51fc661ff5bab9139f7c772f8b459b32ad
pyamsoft <pyam.soft@gmail.com>
2018-08-21 18:56:15 -0700

    Allow access to classes for casting and stream filtering

--- core/src/main/java/com/pyamsoft/pydroid/core/viewmodel/DataWrapper.kt
+++ core/src/main/java/com/pyamsoft/pydroid/core/viewmodel/DataWrapper.kt
@@ -6,10 +6,10 @@ import io.reactivex.Observable
 
 // The common set of activity events
 sealed class DataWrapper<T : Any> {
-  internal class Loading<T : Any> internal constructor(val forced: Boolean) : DataWrapper<T>()
-  internal data class Success<T : Any> internal constructor(val data: T) : DataWrapper<T>()
-  internal data class Error<T : Any> internal constructor(val error: Throwable) : DataWrapper<T>()
-  internal class Complete<T : Any> internal constructor() : DataWrapper<T>()
+  data class Loading<T : Any> internal constructor(val forced: Boolean) : DataWrapper<T>()
+  data class Success<T : Any> internal constructor(val data: T) : DataWrapper<T>()
+  data class Error<T : Any> internal constructor(val error: Throwable) : DataWrapper<T>()
+  class Complete<T : Any> internal constructor() : DataWrapper<T>()
 
   fun onLoading(func: (Boolean) -> Unit) {
     if (this is Loading) {

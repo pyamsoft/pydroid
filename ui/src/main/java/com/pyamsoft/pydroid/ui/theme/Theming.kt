1449ed2e61feceb2396e545e43f9fddb2d50e078
pyamsoft <developer.pyamsoft@gmail.com>
2018-11-12 23:48:57 -0800

    Allow setting theme default

--- ui/src/main/java/com/pyamsoft/pydroid/ui/theme/Theming.kt
+++ ui/src/main/java/com/pyamsoft/pydroid/ui/theme/Theming.kt
@@ -1,27 +1,43 @@
 package com.pyamsoft.pydroid.ui.theme
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.annotation.CheckResult
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 import com.pyamsoft.pydroid.ui.R
+import timber.log.Timber
 
 class Theming internal constructor(context: Context) {
 
-  private val preferences by lazy {
-    PreferenceManager.getDefaultSharedPreferences(context.applicationContext)
+  private val preferences: SharedPreferences
+  private val key: String
+
+  init {
+    val appContext = context.applicationContext
+    preferences = PreferenceManager.getDefaultSharedPreferences(appContext)
+    key = appContext.getString(R.string.dark_mode_key)
+
+    if (!preferences.contains(key)) {
+      setDarkTheme(IS_DEFAULT_DARK_THEME)
+    }
   }
-  private val key by lazy { context.getString(R.string.dark_mode_key) }
-  private val defaultValue by lazy { context.resources.getBoolean(R.bool.dark_mode_default) }
 
   @CheckResult
   fun isDarkTheme(): Boolean {
-    return preferences.getBoolean(key, defaultValue)
+    return preferences.getBoolean(key, IS_DEFAULT_DARK_THEME)
   }
 
   fun setDarkTheme(dark: Boolean) {
     preferences.edit {
+      Timber.d("Set dark theme: $dark")
       putBoolean(key, dark)
     }
+  }
+
+  companion object {
+
+    @JvmField
+    var IS_DEFAULT_DARK_THEME = false
   }
 }

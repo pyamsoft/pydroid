32103485ce1b75e31881c001c927c11a8249cc7d
pyamsoft <developer.pyamsoft@gmail.com>
2018-12-17 17:45:46 -0800

    Use packaged lib for fluid resizer, apply basic to all activities

--- ui/src/main/java/com/pyamsoft/pydroid/ui/app/activity/ActivityBase.kt
+++ ui/src/main/java/com/pyamsoft/pydroid/ui/app/activity/ActivityBase.kt
@@ -16,9 +16,11 @@
 
 package com.pyamsoft.pydroid.ui.app.activity
 
+import android.os.Bundle
 import androidx.annotation.CallSuper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.popinnow.android.fluidresizer.FluidResizer
 import com.pyamsoft.pydroid.ui.app.fragment.BackPressDelegate
 
 abstract class ActivityBase : AppCompatActivity(), ToolbarActivity {
@@ -29,6 +31,12 @@ abstract class ActivityBase : AppCompatActivity(), ToolbarActivity {
     if (!BackPressDelegate.onBackPressed(supportFragmentManager)) {
       super.onBackPressed()
     }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    
+    FluidResizer.listen(this)
   }
 
   @CallSuper
--- ui/src/main/java/com/pyamsoft/pydroid/ui/widget/resize/KeyboardVisibilityChanged.kt
+++
@@ -1,7 +0,0 @@
-package com.pyamsoft.pydroid.ui.widget.resize
-
-data class KeyboardVisibilityChanged internal constructor(
-  val visible: Boolean,
-  val contentHeight: Int,
-  val contentHeightBeforeResize: Int
-)

c07b624da0f66291c8da5036039289fb730f2e93
pyamsoft <developer.pyamsoft@gmail.com>
2019-04-20 21:36:47 -0700

    Null out more on destroy view

--- ui/src/main/java/com/pyamsoft/pydroid/ui/version/upgrade/VersionUpgradeDialog.kt
+++ ui/src/main/java/com/pyamsoft/pydroid/ui/version/upgrade/VersionUpgradeDialog.kt
@@ -35,7 +35,7 @@ import com.pyamsoft.pydroid.ui.util.MarketLinker
 
 class VersionUpgradeDialog : DialogFragment(), VersionUpgradeUiComponent.Callback {
 
-  internal lateinit var component: VersionUpgradeUiComponent
+  internal var component: VersionUpgradeUiComponent? = null
 
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
     return super.onCreateDialog(savedInstanceState)
@@ -64,7 +64,12 @@ class VersionUpgradeDialog : DialogFragment(), VersionUpgradeUiComponent.Callbac
         .create(layoutRoot, latestVersion)
         .inject(this)
 
-    component.bind(viewLifecycleOwner, savedInstanceState, this)
+    requireNotNull(component).bind(viewLifecycleOwner, savedInstanceState, this)
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    component = null
   }
 
   override fun onResume() {
@@ -77,13 +82,13 @@ class VersionUpgradeDialog : DialogFragment(), VersionUpgradeUiComponent.Callbac
 
   override fun onSaveInstanceState(outState: Bundle) {
     super.onSaveInstanceState(outState)
-    component.saveState(outState)
+    requireNotNull(component).saveState(outState)
   }
 
   override fun onNavigateToMarket() {
     val error = MarketLinker.linkToMarketPage(requireContext(), requireContext().packageName)
     if (error != null) {
-      component.navigationFailed(error)
+      requireNotNull(component).navigationFailed(error)
     }
   }
 

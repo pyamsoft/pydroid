dbb43ce8d53664ec5ce579ddfdf9ac8ee5051f74
pyamsoft <developer.pyamsoft@gmail.com>
2021-01-05 19:22:04 -0800

    Docs

--- billing/src/main/java/com/pyamsoft/pydroid/billing/BillingState.kt
+++ billing/src/main/java/com/pyamsoft/pydroid/billing/BillingState.kt
@@ -4,7 +4,18 @@ package com.pyamsoft.pydroid.billing
  * State of the billing client
  */
 public enum class BillingState {
+    /**
+     * Billing client is still loading, state unknown
+     */
     LOADING,
+
+    /**
+     * Billing client is connected and active
+     */
     CONNECTED,
+
+    /**
+     * Billing client is currently disconnected but may become active again later.
+     */
     DISCONNECTED
 }

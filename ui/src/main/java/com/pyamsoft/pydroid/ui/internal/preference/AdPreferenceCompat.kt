4339aab48387aa9eb2f9592407c9d64184accce4
pyamsoft <developer.pyamsoft@gmail.com>
2021-08-24 19:45:13 -0700

    Fix VB crash

--- ui/src/main/java/com/pyamsoft/pydroid/ui/internal/preference/AdPreferenceCompat.kt
+++ ui/src/main/java/com/pyamsoft/pydroid/ui/internal/preference/AdPreferenceCompat.kt
@@ -21,7 +21,6 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.preference.PreferenceViewHolder
 import com.pyamsoft.pydroid.ui.R
-import com.pyamsoft.pydroid.ui.databinding.AdPreferenceLayoutBinding
 import com.pyamsoft.pydroid.ui.preference.PreferenceCompat
 import com.pyamsoft.pydroid.ui.preference.getStyledAttr
 import androidx.preference.R as R2
@@ -55,17 +54,17 @@ internal class AdPreferenceCompat : PreferenceCompat {
 
   override fun onBindViewHolder(holder: PreferenceViewHolder) {
     // Must run before super to fix view IDs
-    val binding = AdPreferenceLayoutBinding.bind(holder.itemView)
-
+    //
     // Fix the IDs here
     // AAPT cannot build when we use @android:id/ inside of an XML layout because
     // of ViewBinding...
-    binding.apply {
-      adIcon.id = android.R.id.icon
-      adSummary.id = android.R.id.summary
-      adTitle.id = android.R.id.title
-      adWidgetFrame.id = android.R.id.widget_frame
-    }
+    //
+    // Expect nullable if the holder has already been "fixed" once before.
+    // Because of this expectation, we cannot use ViewBinding here.
+    holder.findViewById(R.id.ad_icon)?.also { it.id = android.R.id.icon }
+    holder.findViewById(R.id.ad_summary)?.also { it.id = android.R.id.summary }
+    holder.findViewById(R.id.ad_title)?.also { it.id = android.R.id.title }
+    holder.findViewById(R.id.ad_widget_frame)?.also { it.id = android.R.id.widget_frame }
 
     // Now run VH binding
     super.onBindViewHolder(holder)

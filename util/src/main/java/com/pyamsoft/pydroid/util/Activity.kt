49e722f84ec52158973786c5e11dd12c12cd59c2
pyamsoft <developer.pyamsoft@gmail.com>
2022-01-25 20:36:58 -0800

    Add back in the Activity theme bridge
    This can help for pulling out activity theme attributes
    When bridging between the old view system and the new compose theme

---
+++ util/src/main/java/com/pyamsoft/pydroid/util/Activity.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.pydroid.util
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import androidx.annotation.AttrRes
+import androidx.annotation.CheckResult
+
+/** Pulls an attribute from the current Activity theme */
+@CheckResult
+@SuppressLint("ResourceType")
+public fun Activity.valueFromCurrentTheme(@AttrRes attr: Int): Int {
+  return this.valuesFromCurrentTheme(attr)[0]
+}
+
+/** Pulls an attribute from the current Activity theme */
+@CheckResult
+@SuppressLint("ResourceType")
+public fun Activity.valuesFromCurrentTheme(@AttrRes vararg attrs: Int): IntArray {
+  val attributes = this.obtainStyledAttributes(attrs)
+  val styled = IntArray(attrs.size)
+  for (index in attrs.indices) {
+    styled[index] = attributes.getResourceId(index, 0)
+  }
+  attributes.recycle()
+  return styled
+}

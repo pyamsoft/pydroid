a5d7477b77237417c9db1036a2e62d04e733dbb0
pyamsoft <developer.pyamsoft@gmail.com>
2022-05-08 12:44:59 -0700

    Remove color XML

--- ui/src/main/java/com/pyamsoft/pydroid/ui/internal/app/Badges.kt
+++ ui/src/main/java/com/pyamsoft/pydroid/ui/internal/app/Badges.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -67,7 +66,7 @@ private fun Badge(
       modifier =
           modifier
               .background(
-                  brush = SolidColor(colorResource(R.color.green500)),
+                  brush = SolidColor(Color(0xFF4CAF50)),
                   shape = BADGE_SHAPE,
               )
               .padding(

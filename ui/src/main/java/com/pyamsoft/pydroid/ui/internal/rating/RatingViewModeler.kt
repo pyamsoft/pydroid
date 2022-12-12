eaf61adf0d19c0878659e1f717c2a72f052005b8
pyamsoft <developer.pyamsoft@gmail.com>
2022-12-12 12:20:21 -0800

    Spotless and API dump

--- ui/src/main/java/com/pyamsoft/pydroid/ui/internal/rating/RatingViewModeler.kt
+++ ui/src/main/java/com/pyamsoft/pydroid/ui/internal/rating/RatingViewModeler.kt
@@ -18,8 +18,8 @@ package com.pyamsoft.pydroid.ui.internal.rating
 
 import com.pyamsoft.highlander.highlander
 import com.pyamsoft.pydroid.arch.AbstractViewModeler
-import com.pyamsoft.pydroid.bootstrap.rating.rate.AppRatingLauncher
 import com.pyamsoft.pydroid.bootstrap.rating.RatingInteractor
+import com.pyamsoft.pydroid.bootstrap.rating.rate.AppRatingLauncher
 import com.pyamsoft.pydroid.core.Logger
 import com.pyamsoft.pydroid.core.ResultWrapper
 import kotlinx.coroutines.CoroutineScope

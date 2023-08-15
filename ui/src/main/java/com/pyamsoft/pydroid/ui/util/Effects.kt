13b8248ed0a01d4a7ede53512d3241c509856c58
pyamsoft <developer.pyamsoft@gmail.com>
2023-08-15 09:36:23 -0700

    Spotless

--- ui/src/main/java/com/pyamsoft/pydroid/ui/util/Effects.kt
+++ ui/src/main/java/com/pyamsoft/pydroid/ui/util/Effects.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 
 /**

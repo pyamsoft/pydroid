4fb116e9fb4c30980da811530d41a175df35c632
pyamsoft <developer.pyamsoft@gmail.com>
2026-05-13 08:49:49 -0700

    Drop useless tests

--- core/src/test/java/com/pyamsoft/pydroid/core/EnforcerTest.kt
+++ core/src/test/java/com/pyamsoft/pydroid/core/EnforcerTest.kt
@@ -16,7 +16,6 @@
 
 package com.pyamsoft.pydroid.core
 
-import kotlin.test.assertNotNull
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
@@ -27,29 +26,5 @@ public class EnforcerTest {
     val noop = createThreadEnforcer(debug = false)
     noop.assertOnMainThread()
     noop.assertOffMainThread()
-  }
-
-  @Test
-  public fun real_throwsOn(): Unit = runTest {
-    val noop = createThreadEnforcer(debug = true)
-
-    try {
-      noop.assertOnMainThread()
-    } catch (e: Throwable) {
-      assertNotNull(e.message)
-      assert(e.message!!.startsWith("Method getMainLooper in android.os.Looper not mocked"))
-    }
-  }
-
-  @Test
-  public fun real_throwsOff(): Unit = runTest {
-    val noop = createThreadEnforcer(debug = true)
-
-    try {
-      noop.assertOffMainThread()
-    } catch (e: Throwable) {
-      assertNotNull(e.message)
-      assert(e.message!!.startsWith("Method getMainLooper in android.os.Looper not mocked"))
-    }
   }
 }
